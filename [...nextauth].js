f655be0f86efb696354da1970543ce9bfddcbc93
Gagan Reddy Mallam <99109231+GaganReddyin@users.noreply.github.com>
2024-09-15 12:04:28 +0530

    Create [...nextauth].js

---
+++ [...nextauth].js
@@ -0,0 +1,23 @@
+import NextAuth from 'next-auth'
+import GithubProvider from '@auth/github-provider'
+
+export default NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token
+      }
+      return token
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken
+      return session
+    },
+  },
+})
